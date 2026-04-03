aaf0fbbc3fd9264c319d8606f5168d3d64ca657d
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-04-03 16:19:35 +0500

    done images module

--- main.py
+++ main.py
@@ -10,12 +10,24 @@ def make_folder(source):
         except:
             print("The folder already exists with this name select a different name")
 
+def image_organization(file , source , destination):
+    image_folder = os.path.join(destination, "Image")
+    if os.path.exists(image_folder):
+        orginal_path = os.path.join(source , file)
+        filename = os.path.basename(orginal_path)
+        os.replace(orginal_path , os.path.join(image_folder , filename))
+    else:
+        os.mkdir(image_folder)
+        image_organization(file , source , destination)
+
 def file_organizer(source):
     destination = make_folder(source)
-
     files = os.listdir(source)
+    images_ext = ("jpeg" , "png" , "jpg" , "webp")
+    for file in files:
+        if file.endswith(tuple(images_ext)):
+            image_organization(file , source , destination)
     
-
 def main():
     while True:
         source = input("Enter the path of your messey folder: ")
